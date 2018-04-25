1343c9b4cf3538afd3e961618bdfbf055b7978e7
Andy Edmonds <edmo@zhaw.ch>
2018-04-25 17:07:25 +0200

    minor changes to startup script

--- kbuild/bootstrap.sh
+++ kbuild/bootstrap.sh
@@ -4,7 +4,7 @@ set -x
 # Set env vars
 ADMIN_TOKEN=${ADMIN_TOKEN:-token}
 ADMIN_TENANT_NAME=${ADMIN_TENANT_NAME:-admin}
-ADMIN_USER_NAME=${ADMIN_USERNAME:-admin}
+ADMIN_USER_NAME=${ADMIN_USER_NAME:-admin}
 ADMIN_PASSWORD=${ADMIN_PASSWORD:-admin}
 ADMIN_ROLE=${ADMIN_ROLE:-admin}
 ADMIN_EMAIL=${ADMIN_EMAIL:-${ADMIN_USER_NAME}@zhaw.ch}
@@ -17,16 +17,18 @@ KEYSTONE_API_VERSION=${KEYSTONE_API_VERSION:-3}
 CONFIG_FILE=/etc/keystone/keystone.conf
 SQL_SCRIPT=${SQL_SCRIPT:-/root/keystone.sql}
 
-if env | grep -qi MYSQL_ROOT_PASSWORD && test -e $SQL_SCRIPT; then
+if env | grep -qi MYSQL_ROOT_PASSWORD && test -e ${SQL_SCRIPT}; then
     sleep 10 # wait for start of mysql
     MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
     MYSQL_HOST=${MYSQL_HOST:-mysql}
-    sed -i "s#^connection.*=.*#connection = mysql+pymysql://keystone:${KEYSTONE_DB_PWD}@${MYSQL_HOST}/keystone#" $CONFIG_FILE
-    sed -i -- "s/KEYSTONE_DBPASS/${KEYSTONE_DB_PWD}/g" $SQL_SCRIPT
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST <$SQL_SCRIPT
+    sed -i "s#^connection.*=.*#connection = mysql+pymysql://keystone:${KEYSTONE_DB_PWD}@${MYSQL_HOST}/keystone#" ${CONFIG_FILE}
+    cat ${CONFIG_FILE}
+    sed -i "s/KEYSTONE_DBPASS/${KEYSTONE_DB_PWD}/g" ${SQL_SCRIPT}
+    cat ${SQL_SCRIPT}
+    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} < ${SQL_SCRIPT}
 fi
 
-rm -f $SQL_SCRIPT
+rm -f ${SQL_SCRIPT}
 
 # Create DB tables
 keystone-manage db_sync
@@ -50,7 +52,7 @@ keystone-manage bootstrap \
     --bootstrap-internal-url http://${HOSTNAME}:5000
 
 # Write openrc to disk
-cat >~/keystone_admin_openrc <<EOF
+cat >~/keystone_admin_openrc << EOF
 export OS_PROJECT_DOMAIN_NAME=${KEYSTONE_DOMAIN}
 export OS_USER_DOMAIN_NAME=${KEYSTONE_DOMAIN}
 export OS_PROJECT_NAME=${ADMIN_TENANT_NAME}
